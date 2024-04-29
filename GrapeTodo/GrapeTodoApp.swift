dfc993e4785b573cdc20613e846a36f8afc7cb1f
changhyen yun <ychcom@naver.com>
2024-04-29 12:02:52 +0900

    Initial Commit

---
+++ GrapeTodo/GrapeTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrapeTodoApp.swift
+//  GrapeTodo
+//
+//  Created by changhyen yun on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GrapeTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
